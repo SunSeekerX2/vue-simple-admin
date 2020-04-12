c2a51f54eaed364eda8b5209784224e668c03359
SunSeekerX <1647800606@qq.com>
2020-04-13 00:23:00 +0800

    feat: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,27 @@
  * @author: SunSeekerX
  * @Date: 2020-04-07 17:00:35
  * @LastEditors: SunSeekerX
- * @LastEditTime: 2020-04-10 00:02:10
+ * @LastEditTime: 2020-04-12 22:41:08
  */
 
 module.exports = {
   base: '/doc/',
   title: 'vue-simple-admin',
-  description: 'A simple vue admin template with Element UI & axios & iconfont'
+  description: '王彬的前端技术文档',
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  plugins: [
+    '@vuepress/last-updated',
+    {
+      transformer: (timestamp, lang) => {
+        // Don't forget to install moment yourself
+        const moment = require('moment')
+        moment.locale(lang)
+        return moment(timestamp).fromNow()
+      }
+    },
+    '@vuepress/active-header-links',
+    '@vuepress/nprogress'
+  ]
 }
