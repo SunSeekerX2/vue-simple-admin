a48aa4500c3f4e3d72fa0f54677f87dfbaeb7783
SunSeekerX <1647800606@qq.com>
2020-04-27 13:04:07 +0800

    feat: remove useless dependencies

--- src/router/index.js
+++ src/router/index.js
@@ -34,13 +34,13 @@ export const constantRoutes = [
   {
     path: '/login',
     component: () => import('@/views/login/index'),
-    hidden: true
+    hidden: true,
   },
 
   {
     path: '/404',
     component: () => import('@/views/404/index'),
-    hidden: true
+    hidden: true,
   },
 
   {
@@ -52,9 +52,9 @@ export const constantRoutes = [
         path: 'dashboard',
         name: 'Dashboard',
         component: () => import('@/views/dashboard/index'),
-        meta: { title: 'Dashboard', icon: 'dashboard' }
-      }
-    ]
+        meta: { title: 'Dashboard', icon: 'dashboard' },
+      },
+    ],
   },
 
   {
@@ -68,21 +68,33 @@ export const constantRoutes = [
         path: 'table',
         name: 'Table',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: 'Table', icon: 'table' },
       },
       {
         path: 'tree',
         name: 'Tree',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
+        meta: { title: 'Tree', icon: 'tree' },
       },
       {
         path: 'index',
         name: 'Form',
         component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
+        meta: { title: 'Form', icon: 'form' },
+      },
+      {
+        path: '404',
+        name: 'Test404',
+        component: () => import('@/views/404/index'),
+        meta: { title: '404', icon: 'table' },
+      },
+      {
+        path: '500',
+        name: '500',
+        component: () => import('@/views/example/500/index'),
+        meta: { title: '500', icon: 'table' },
+      },
+    ],
   },
 
   {
@@ -95,15 +107,9 @@ export const constantRoutes = [
         path: 'index',
         name: 'TestIndex',
         component: () => import('@/views/test/index'),
-        meta: { title: 'Test', icon: 'form' }
+        meta: { title: 'Test', icon: 'form' },
       },
-      {
-        path: '404',
-        name: 'Test404',
-        component: () => import('@/views/404/index'),
-        meta: { title: '404', icon: 'form' }
-      }
-    ]
+    ],
   },
 
   {
@@ -113,7 +119,7 @@ export const constantRoutes = [
     name: 'Nested',
     meta: {
       title: 'Nested',
-      icon: 'nested'
+      icon: 'nested',
     },
     children: [
       {
@@ -126,7 +132,7 @@ export const constantRoutes = [
             path: 'menu1-1',
             component: () => import('@/views/nested/menu1/menu1-1'),
             name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
+            meta: { title: 'Menu1-1' },
           },
           {
             path: 'menu1-2',
@@ -139,42 +145,42 @@ export const constantRoutes = [
                 component: () =>
                   import('@/views/nested/menu1/menu1-2/menu1-2-1'),
                 name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
+                meta: { title: 'Menu1-2-1' },
               },
               {
                 path: 'menu1-2-2',
                 component: () =>
                   import('@/views/nested/menu1/menu1-2/menu1-2-2'),
                 name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
+                meta: { title: 'Menu1-2-2' },
+              },
+            ],
           },
           {
             path: 'menu1-3',
             component: () => import('@/views/nested/menu1/menu1-3'),
             name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
+            meta: { title: 'Menu1-3' },
+          },
+        ],
       },
       {
         path: 'menu2',
         component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
-      }
-    ]
+        meta: { title: 'menu2' },
+      },
+    ],
   },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
+  { path: '*', redirect: '/404', hidden: true },
 ]
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes
+    routes: constantRoutes,
   })
 
 const router = createRouter()
